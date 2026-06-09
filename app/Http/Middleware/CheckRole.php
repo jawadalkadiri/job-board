97719d9ae56b8a412b6f80e2dc71f94f613480f5
JawadKadery <jawadalkadiri@gmail.com>
2026-06-09 12:16:57 +0300

    new

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class CheckRole
 {
@@ -13,8 +15,12 @@ class CheckRole
      *
      * @param  Closure(Request): (Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, string $role): Response
     {
+
+        if(!Auth::check() || Auth::user()->role !== $role) {
+            abort(403, 'Unauthorized action.');
+        }
         return $next($request);
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\CheckRole;
+
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias(['checkRole' => App\Http\Middleware\CheckRole::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
